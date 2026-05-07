a8ed098b00c475873738df7779156c4893c29c58
Kipkorir2017 <pkobenj41@gmail.com>
2026-05-07 19:45:12 +0300

    edit the mainLayout file

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,51 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import { createBrowserRouter } from "react-router-dom";
 
-const MainLayout = ({ children }) => {
-  return (
-    <div>
-      <Navbar />
-      <main>{children}</main>
-      <Footer />
-    </div>
-  );
-};
+import MainLayout from "../layouts/MainLayout";
 
-export default MainLayout;
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Services from "../pages/Services";
+import Projects from "../pages/Projects";
+import SpareParts from "../pages/SpareParts";
+import Contact from "../pages/Contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+
+      {
+        path: "/about",
+        element: <About />,
+      },
+
+      {
+        path: "/services",
+        element: <Services />,
+      },
+
+      {
+        path: "/projects",
+        element: <Projects />,
+      },
+
+      {
+        path: "/spare-parts",
+        element: <SpareParts />,
+      },
+
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+export default router;
